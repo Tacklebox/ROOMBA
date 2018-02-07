7eef206536b04b3900f2f72564c48665e78453d9
Maxwell Borden <maxwellborden@gmail.com>
2018-02-06 16:29:14 -0800

    Added the TTA files for phase 2

--- src/ROOMBA.ino
+++ src/ROOMBA.ino
@@ -4,14 +4,27 @@
 #include <Wire.h>
 #include <hd44780.h>
 #include <hd44780ioClass/hd44780_I2Cexp.h>
+#include <Servo.h>
 
 hd44780_I2Cexp lcd;
 
 const int LCD_COLS = 16;
 const int LCD_ROWS = 2;
+Servo servo_x;
+Servo servo_y;
+int pos_x = 0;
+int pos_y = 0;
+int axis_x = A0;
+int axis_y = A1;
+int clicker = 50;
+int laser = 13;
 
 void setup()
 {
+  servo_1.attach(9);
+  servo_2.attach(10);
+	pinMode(clicker, INPUT);
+	pinMode(laser, OUTPUT);
   int status;
   status = lcd.begin(LCD_COLS, LCD_ROWS);
   if(status)
@@ -22,4 +35,14 @@ void setup()
   lcd.print("Hello, World!");
 }
 
-void loop() {}
+void loop() {
+  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+}
