b03ad0aec8f10d664ed96656e653f0c4a0e90d5b
Maxwell Borden <maxwellborden@gmail.com>
2018-04-13 09:02:26 -0700

    added UART lib

--- rtos/lib/messaging/messaging.h
+++ rtos/lib/messaging/messaging.h
@@ -1,8 +1,16 @@
-typedef __attribute__((__packed__)) union message_type {
-	struct {
-		uint16_t x_pos;
-		uint16_t y_pos;
-	}
-	char[] bytes;
+#include <stdint.h>
+
+typedef union message_type {
+    struct {
+        char magic: 7;
+        char btn:   1;
+        uint16_t x_pos_1;
+        uint16_t y_pos_1;
+        uint16_t x_pos_2;
+        uint16_t y_pos_2;
+    } __attribute__((__packed__)) ;
+    char bytes[9];
 } message;
 
+message recv_message_bt();
+void send_message_bt(message m);
