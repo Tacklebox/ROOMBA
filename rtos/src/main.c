8e7548ece0e90151e55fe9b011760ecf66ff774e
aleksiy325 <aleksiy123@gmail.com>
2018-04-14 16:12:49 -0700

    add wall

--- rtos/src/main.c
+++ rtos/src/main.c
@@ -93,15 +93,13 @@ void sample_roomba_sensor_data() {
 
 void backup(){
     Roomba_Drive(1 * MOVE_SPEED, 0);
-    _delay_ms(500);
+    _delay_ms(300);
     Roomba_Drive(0, 0);
 }
 
 void process_roomba_sensor_data() {
-    if(roomba_data.virtual_wall || roomba_data.wall){
-        roomba_data.virtual_wall = 0;
-        roomba_data.wall = 0;
-        //Task_Create_System(backup, 0);
+    if(roomba_data.virtual_wall || roomba_data.wall || roomba_data.bumps_wheeldrops > 0){
+        Task_Create_System(backup);
     }
 }
 
@@ -181,7 +179,7 @@ int main() {
     OS_Init();
     // Task_Create_Period(joystick_task, 0, 20, 1, 2);
     Task_Create_Period(roomba_sensor_task, 0, 100, 80, 0);
-    // Task_Create_Period(ir_sensor_task, 0, 50, 20, 5);
+    //Task_Create_Period(ir_sensor_task, 0, 50, 20, 5);
     // Task_Create_Period(swap_modes, 0, 6000, 1, 0);
     OS_Start();
     return 1;
